e811e4c316887820b6df6ffc5fa4df05ea7da275
Joshua OLADIPO <Joshua OLADIPO@LAPTOP-T24PK5OM>
2022-07-29 17:28:53 -0700

    Add project files.

---
+++ MyFirstWebAPI/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return Ok("Successfully Login");
+        }
+    }
+}
